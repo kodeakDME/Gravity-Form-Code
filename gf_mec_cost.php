845dd3b3e98f8a71ccbf1b984da280d6e416a599
Kodeak <79168160+kodeakDME@users.noreply.github.com>
2021-11-03 11:38:39 -0700

    Update gf_mec_cost.php

--- gf_mec_cost.php
+++ gf_mec_cost.php
@@ -1,4 +1,5 @@
 <?php
+// use the parameter after 'gform_field_value_{Your parameter call here}'
 
 add_filter( 'gform_field_value_event_cost', 'populate_event_cost' );
 function populate_event_cost( $value ) {
